dc92131fa455d5c2030fede658cb1912bed99783
Dan Engel <dengel29@gmail.com>
2018-05-18 16:39:00 +0800

    added the current_user to books

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // //app.js
 App({
   onLaunch: function () {
-    const host = 'http://localhost:3000/api/v1/'
     console.log('processing to login') 
     wx.login({
       success: res => {
         console.log(res)
 
         wx.request({
-          url: host + 'login', 
+          url: this.globalData.host + 'login', 
           method: 'post', 
           data: {
             code: res.code
@@ -22,8 +21,7 @@ App({
     })
   },
   globalData: {
-
-  
+    host: 'http://localhost:3000/api/v1/'
   }
 })
   
